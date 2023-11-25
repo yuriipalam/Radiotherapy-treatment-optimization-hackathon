cbd8f361b4a4908aa0f5a565cdbace8396ad4d57
GeorgiyIakunenko <Goshan3097@gmail.com>
2023-11-25 20:19:49 +0100

    footer added

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="py-4 ">
+    <div className="py-4 text-sm text-center bg-neutral-50">
       © 1999-2023 Varian Medical Systems, Inc. All Rights reserved.
     </div>
   );
--- web/src/layout.tsx
+++ web/src/layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header.tsx";
+import { Footer } from "@/components/Footer.tsx";
 
 export default function RootLayout() {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
